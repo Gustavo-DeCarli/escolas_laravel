5d0f861a002c6e8d84abdf10b40f43b42d82f85a
trollgugui13 <84552703+trollgugui13@users.noreply.github.com>
2022-07-22 09:15:04 -0300

    Delete escolas directory

--- escolas/cursos/cursoshow.blade.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li>ID: {{$curso->id}}</li>
-    <li>Nome: {{$curso->nome}}</li>
-    <li>Nome Reduzido: {{$curso->nome_reduzido}}</li>
-</ul>
-<a href="/escolas">Voltar</a>
--- escolas/formularios/formulariosshow.blade.php
+++
@@ -1,7 +0,0 @@
-<ul>
-    <li>ID: {{$formulario->id}}</li>
-    <li>Ano: {{$formulario->ano}}</li>
-    <li>Data de início: {{$formulario->dt_inicio}}</li>
-    <li>Data de término: {{$formulario->dt_fim}}</li>
-</ul>
-<a href="/escolas">Voltar</a>
